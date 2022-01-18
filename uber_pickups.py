869f1b1ac18632f66493f4e38887ea061b3d7fbb
Ekaterina <80875367+EkaterinaVZ@users.noreply.github.com>
2022-01-18 23:29:36 +0500

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,6 +1,5 @@
 """
-# My first app
-Here's our first attempt at using data to create a table:
+# Our first app
 """
 
 import streamlit as st
@@ -13,19 +12,13 @@ st.image("logotip.png", width=400)
 
 st.title('Model "Fill mask"')
 
-inp = st.text_input('Введите текст на языке согласно представленному ниже образцу',
+inp = st.text_input('Введите текст на английском языке согласно представленному ниже образцу',
                     'I study economics at [MASK].')
 
 unmasker = pipeline('fill-mask', model='albert-base-v2')
 
 if inp:
     text = unmasker(inp)
-    print(inp)
-    print(text)
     st.write("Возможныe варианты ответа:")
     for el in text:
-        print(el)
         st.write(el["sequence"])
-
-
-# streamlit run C:/Users/Админ/PycharmProjects/fill-mask/uber_pickups.py
